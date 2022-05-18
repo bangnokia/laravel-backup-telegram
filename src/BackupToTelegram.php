c18ae2fc5bdd5a15532f9e34e4530e045ffa39bc
bangnokia <bangnokia@gmail.com>
2022-05-18 15:40:38 +0700

    work in progress

---
+++ src/BackupToTelegram.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Bangnokia\LaravelBackupTelegram;
+
+use Illuminate\Support\Facades\Http;
+use Spatie\Backup\Events\BackupWasSuccessful;
+
+class BackupToTelegram
+{
+    protected string $token;
+    protected string $chatId;
+
+    public function __construct()
+    {
+        $this->token = config('services.telegram-bot-api.token');
+        $this->chatId = config('services.telegram-bot-api.chat_id');
+    }
+
+    public function handle(BackupWasSuccessful $event): void
+    {
+        $backup = $event->backupDestination->newestBackup();
+
+        $response = Http::attach('document', $backup->disk()->get($backup->path()), $backup->path())
+            ->post(
+                "https://api.telegram.org/bot{$this->token}/sendDocument",
+                ['chat_id' => $this->chatId, 'caption' => config('app.name')]
+            );
+
+        if ($response['ok']) {
+            consoleOutput()->info('Upload to telegram successfully!');
+        } else {
+            consoleOutput()->error('Can not upload to telegram!');
+        }
+    }
+}
--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,7 +2,18 @@
 
 namespace Bangnokia\LaravelBackupTelegram;
 
-class ServiceProvider
-{
+use Illuminate\Support\Facades\Event;
+use Spatie\Backup\Events\BackupWasSuccessful;
 
+class ServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        Event::listen(BackupWasSuccessful::class, [BackupToTelegram::class, 'handle']);
+    }
 }

e421c4f516ae3f9e0c0e6135d0c28d431dfb675d
bangnokia <bangnokia@gmail.com>
2023-04-05 10:23:38 +0700

    wip

--- src/BackupToTelegram.php
+++ src/BackupToTelegram.php
@@ -3,6 +3,7 @@
 namespace Bangnokia\LaravelBackupTelegram;
 
 use Illuminate\Support\Facades\Http;
+use Spatie\Backup\BackupDestination\Backup;
 use Spatie\Backup\Events\BackupWasSuccessful;
 
 class BackupToTelegram
@@ -21,15 +22,48 @@ class BackupToTelegram
     {
         $backup = $event->backupDestination->newestBackup();
 
-        $response = Http::attach('document', $backup->disk()->get($backup->path()), $backup->path())
-            ->post(
-                "https://api.telegram.org/bot{$this->token}/sendDocument",
-                ['chat_id' => $this->chatId, 'caption' => config('app.name')]
-            );
-
+        // if file size bigger than 50MB, chunk the files into multiple parts
+        if ($backup->sizeInBytes() > 50 * 1024 * 1024) {
+            $response = $this->splitAndUpload($backup, 50);
+        } else {
+            $response = $this->singleUpload($backup);
+        }
 
         return $response['ok']
             ? consoleOutput()->info('Uploaded to Telegram successful!')
             : consoleOutput()->error('Can not upload to Telegram');
+    }
+
+    public function singleUpload(Backup $backup): array
+    {
+        $response = Http::attach('document', $backup->disk()->get($backup->path()), $backup->path())
+            ->post(
+                "https://api.telegram.org/bot{$this->token}/sendDocument",
+                ['chat_id' => $this->chatId, 'caption' => config('app.name')]
+            )->throw();
+
+        return $response->json();
+    }
+
+    public function splitAndUpload(?Backup $backup): array
+    {
+        $sword = new Sword();
+        // chunk the file into 50MB parts
+        $parts = $sword->slash($backup->disk()->path($backup->path()));
+
+        foreach ($parts as $part) {
+            $response = Http::attach('document', $part, basename($part))
+                ->post(
+                    "https://api.telegram.org/bot{$this->token}/sendDocument",
+                    ['chat_id' => $this->chatId, 'caption' => config('app.name')]
+                )->throw();
+        }
+
+        // delete the parts
+        foreach ($parts as $part) {
+            unlink($part);
+        }
+
+        return $response->json(); // return the last response
     }
 }
---
+++ tests/SwordTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Bangnokia\LaravelBackupTelegram\Tests;
+
+use Bangnokia\LaravelBackupTelegram\Sword;
+
+class SwordTest extends TestCase
+{
+    /** @test */
+    public function it_could_split_file_into_chunks()
+    {
+        $sword = new Sword();
+        $testFile = __DIR__ . '/dummy/test.zip';
+
+        $parts = $sword->slash($testFile, 1);
+
+        $this->assertIsArray($parts);
+        $this->assertCount(2, $parts);
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $parts = glob(__DIR__ . '/dummy/test.zip.part*');
+        foreach ($parts as $part) {
+            unlink($part);
+        }
+    }
+}
